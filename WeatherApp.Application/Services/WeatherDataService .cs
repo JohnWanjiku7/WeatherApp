d473af5a7a6c89651cddd84c5095c36a79bf2272
johnkagwanja77 <3r7jiywaxf3zpx6632jdq45n4bfp4hgspb7mpib3wpyjlzbxmybq>
2023-10-11 20:40:18 +0300

    Optimise code in weather app program file by adding nmethds and configuring the service collection Improve Pdf Generator by adding methods

--- WeatherApp.Application/Services/WeatherDataService .cs
+++ WeatherApp.Application/Services/WeatherDataService .cs
@@ -29,12 +29,10 @@ namespace WeatherApp.Application.Services
 
             serviceCollection.AddSingleton<IConfiguration>(configuration);
 
-            string apiKey = (configuration["WeatherApi:ApiKey"]);
-            string apiUrl = (configuration["WeatherApi:EndpointUrl"]);
+          
 
-         
-            _apiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
-            _apiUrl = apiUrl ?? throw new ArgumentNullException(nameof(apiUrl));
+            _apiKey = configuration["WeatherApi:ApiKey"] ?? throw new ArgumentNullException("ApiKey is missing in appsettings.json");
+            _apiUrl = configuration["WeatherApi:EndpointUrl"] ?? throw new ArgumentNullException("EndpointUrl is missing in appsettings.json");
         }
 
         public async Task<WeatherResponse> GetWeatherDataAsync(string locationName)
