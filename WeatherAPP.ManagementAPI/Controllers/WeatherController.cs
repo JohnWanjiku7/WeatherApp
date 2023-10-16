25da421e36cf454e593a6c2195448b891e5358a2
johnkagwanja77 <3r7jiywaxf3zpx6632jdq45n4bfp4hgspb7mpib3wpyjlzbxmybq>
2023-10-16 09:50:01 +0300

    Remove unsued classes

--- WeatherAPP.ManagementAPI/Controllers/WeatherController.cs
+++ WeatherAPP.ManagementAPI/Controllers/WeatherController.cs
@@ -13,27 +13,27 @@ namespace WeatherAPP.ManagementAPI.Controllers
     public class WeatherController : Controller
     {
 
-        private readonly IWeatherStackAPI _weatherApi;
+       
         private readonly IMediator _Mediator;
-       public WeatherController(IWeatherStackAPI weatherStackAPI, IMediator mediator)
+       public WeatherController( IMediator mediator)
         {
-            _weatherApi = weatherStackAPI;
+          
             _Mediator = mediator;
         }
-        [HttpGet]
+        //[HttpGet]
 
-        public async Task<ActionResult<WeatherResponseDto>> GetWeather(string name)
-        {
-            var weather = await _Mediator.Send(new GetWeatherDataRequest{ locationName = name });
-            return weather;
-        }
+        //public async Task<ActionResult<WeatherResponseDto>> GetWeather(string name)
+        //{
+        //    var weather = await _Mediator.Send(new GetWeatherDataRequest{ locationName = name });
+        //    return weather;
+        //}
         [Route("/PDF")]
         [HttpGet]
         public async Task<ActionResult<WeatherResponseDto>> GetPDF(string name)
         {
             WeatherResponseDto weatherDto = await _Mediator.Send(new GetWeatherDataRequest { locationName = name });
            
-            //var request = new GeneratePdfRequest(/* parameters */);
+         
 
             byte[] pdfContent = await _Mediator.Send(new GeneratePDFRequest { weatherResponseDto = weatherDto });
 
@@ -44,29 +44,7 @@ namespace WeatherAPP.ManagementAPI.Controllers
             }
             return BadRequest("PDF generation failed.");
         }
-        //public async Task<WeatherResponseDto> Get(string LocationName)
-        //{
-        //    try
-        //    {
-        //        // Use the injected WeatherDataService to fetch weather data
-        //        WeatherResponseDto weatherData = await _weatherApi.GetWeatherDataAsync(LocationName);
-
-        //        //if (weatherData == null)
-        //        //{
-
-        //        //    throw new WeatherDomainException("\nFetch Weather Data Use Case Error", "Weather data received is null");
-        //        //}
-
-        //        return weatherData;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Handle exceptions related to weather data fetching
-        //        //throw new WeatherDomainException("\nFetch Weather Data Use Case Error", ex.Message);
-        //        return null;
-        //    }
-        //}
-
+        
 
     }
 }
